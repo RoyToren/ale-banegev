7c7e35c1ebce8099cd3bfbc1da047e70b699bc40
roy <k@k>
2017-08-29 01:22:31 +0300

    updating

--- app/createUser/createUserCtrl.js
+++ app/createUser/createUserCtrl.js
@@ -2,73 +2,76 @@
 
 // Register `phoneList` component, along with its associated controller and template
 angular.
-  module('createUser').
-  component('createUser', {
-    templateUrl: 'createUser/createUser.html',
-    controller: ['$scope',
-      function CreateUserController($scope) {
-        $scope.steps = [
-                'שלב 4: תחומי עניין',
-                'שלב 3: סיכונים',
-                'שלב 2: יכולות',
-                'שלב 1: נתונים כללים',
-              ];
-        $scope.AddRisk = function(RiskData)
-        {
-                if($scope.risks == null)
-                {
-                        $scope.risks = [];
+    module('createUser').
+    component('createUser', {
+        templateUrl: 'createUser/createUser.html',
+        controller: ['$scope',
+            function CreateUserController($scope) {
+                $scope.User = {
+                    id : ""
+                 };
+                $scope.steps = [
+                    'שלב 4: תחומי עניין',
+                    'שלב 3: סיכונים',
+                    'שלב 2: יכולות',
+                    'שלב 1: נתונים כללים',
+                ];
+                $scope.AddRisk = function (RiskData) {
+                    if ($scope.User.risks == null) {
+                        $scope.User.risks = [];
+                    }
+                    $scope.User.risks.push(angular.copy(RiskData));
+                    RiskData.name= "";
+                    RiskData.prevention = "";
                 }
-               $scope.risks.push(angular.copy(RiskData)); 
-               RiskData.name = "";
-               RiskData.prevent = "";
-        }
-              $scope.selection = $scope.steps[3];
-            
-              $scope.getCurrentStepIndex = function(){
-                // Get the index of the current step given selection
-                return _.indexOf($scope.steps, $scope.selection);
-              };
-            
-              // Go to a defined step index
-              $scope.goToStep = function(index) {
-                if ( !_.isUndefined($scope.steps[index]) )
-                {
-                  $scope.selection = $scope.steps[index];
+                $scope.createUser = function () {
+                    var x = "kaki";
+                    $scope.kaki = {};
+                    x++;
                 }
-              };
-            
-              $scope.hasNextStep = function(){
-                var stepIndex = $scope.getCurrentStepIndex();
-                var nextStep = stepIndex + 1;
-                // Return true if there is a next step, false if not
-                return !_.isUndefined($scope.steps[nextStep]);
-              };
-            
-              $scope.hasPreviousStep = function(){
-                var stepIndex = $scope.getCurrentStepIndex();
-                var previousStep = stepIndex - 1;
-                // Return true if there is a next step, false if not
-                return !_.isUndefined($scope.steps[previousStep]);
-              };
-            
-              $scope.incrementStep = function() {
-                if ( $scope.hasNextStep() )
-                {
-                  var stepIndex = $scope.getCurrentStepIndex();
-                  var nextStep = stepIndex + 1;
-                  $scope.selection = $scope.steps[nextStep];
-                }
-              };
-            
-              $scope.decrementStep = function() {
-                if ( $scope.hasPreviousStep() )
-                {
-                  var stepIndex = $scope.getCurrentStepIndex();
-                  var previousStep = stepIndex - 1;
-                  $scope.selection = $scope.steps[previousStep];
-                }
-              };
-      }
-    ]
-});
+                $scope.selection = $scope.steps[3];
+
+                $scope.getCurrentStepIndex = function () {
+                    // Get the index of the current step given selection
+                    return _.indexOf($scope.steps, $scope.selection);
+                };
+
+                // Go to a defined step index
+                $scope.goToStep = function (index) {
+                    if (!_.isUndefined($scope.steps[index])) {
+                        $scope.selection = $scope.steps[index];
+                    }
+                };
+
+                $scope.hasNextStep = function () {
+                    var stepIndex = $scope.getCurrentStepIndex();
+                    var nextStep = stepIndex + 1;
+                    // Return true if there is a next step, false if not
+                    return !_.isUndefined($scope.steps[nextStep]);
+                };
+
+                $scope.hasPreviousStep = function () {
+                    var stepIndex = $scope.getCurrentStepIndex();
+                    var previousStep = stepIndex - 1;
+                    // Return true if there is a next step, false if not
+                    return !_.isUndefined($scope.steps[previousStep]);
+                };
+
+                $scope.incrementStep = function () {
+                    if ($scope.hasNextStep()) {
+                        var stepIndex = $scope.getCurrentStepIndex();
+                        var nextStep = stepIndex + 1;
+                        $scope.selection = $scope.steps[nextStep];
+                    }
+                };
+
+                $scope.decrementStep = function () {
+                    if ($scope.hasPreviousStep()) {
+                        var stepIndex = $scope.getCurrentStepIndex();
+                        var previousStep = stepIndex - 1;
+                        $scope.selection = $scope.steps[previousStep];
+                    }
+                };
+            }
+        ]
+    });
