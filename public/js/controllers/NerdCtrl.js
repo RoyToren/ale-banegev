235d3487a1d611cc6462826553e129ec169251c5
GALCOHENLAPTOP\GalCohen <2galcohen2@gmail.com>
2017-08-28 16:41:28 +0300

    מסך דיירים

--- public/js/controllers/NerdCtrl.js
+++ public/js/controllers/NerdCtrl.js
@@ -1,5 +1,14 @@
 angular.module('NerdCtrl', []).controller('NerdController', function($scope) {
 
 	$scope.tagline = 'Nothing beats a pocket protector!';
+	$scope.roomers = [
+		{name:"משה כהן", age:14, info:"אלרגי לבוטנים"},
+		{name:"אבי לוי", age:17, info:"צריך עזרה להשתמש בשירותים"},
+		{name:"דני דין", age:22, info:"נעלם מידי פעם"}
+	];
 
+	$scope.RoomerSelected = function(roomer)
+	{
+		alert(roomer.name);
+	}
 });
