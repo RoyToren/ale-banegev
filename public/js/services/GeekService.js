dde6e1c104e64286bce109cd2a909c4c1d7e5629
Gal-PC\Gal <galdayan1895@gmail.com>
2017-08-28 17:31:37 +0300

    no message

--- public/js/services/GeekService.js
+++ public/js/services/GeekService.js
@@ -1,5 +1,8 @@
 angular.module('GeekService', []).factory('Geek', ['$http', function($http) {
-
+    $http.get("welcome.htm")
+    .then(function(response) {
+        $scope.docs = response.docs;
+    });
 	
 
 }]);
