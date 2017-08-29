be3a60843e109143e4e3642cb87407603d345cc9
Gal-PC\Gal <galdayan1895@gmail.com>
2017-08-29 12:30:20 +0300

    no message

--- app/rooms-list/rooms-list.component.js
+++ app/rooms-list/rooms-list.component.js
@@ -57,43 +57,43 @@ component('roomsList', {
 
             switch ((j + 1) % scope.numPerPage) {
               case 1:
-                renderedUser.background = "rgba(255,0,0,0.7)";
+                renderedUser.background = "rgba(255,0,0,0.2)";
                 renderedUser.span.row = renderedUser.span.col = 2;
                 break;
 
               case 2:
-                renderedUser.background = "rgba(0,128,0,0.7)";
+                renderedUser.background = "rgba(0,128,0,0.2)";
                 break;
               case 3:
-                renderedUser.background = "rgba(0,0,255,0.7)";
+                renderedUser.background = "rgba(0,0,255,0.2)";
                 break;
               case 4:
-                renderedUser.background = "rgba(0,0,139,0.7)";
+                renderedUser.background = "rgba(0,0,139,0.2)";
                 renderedUser.span.col = 2;
                 break;
 
               case 5:
-                renderedUser.background = "rgba(237,237,79,0.7)";
+                renderedUser.background = "rgba(237,237,79,0.2)";
                 renderedUser.span.row = renderedUser.span.col = 2;
                 break;
 
               case 6:
-                renderedUser.background = "rgba(255,192,203,0.7)";
+                renderedUser.background = "rgba(255,192,203,0.2)";
                 break;
               case 7:
-                renderedUser.background = "rgba(255,0,0,0.7)";
+                renderedUser.background = "rgba(255,0,0,0.2)";
                 break;
               case 8:
-                renderedUser.background = "rgba(128,0,128,0.7)";
+                renderedUser.background = "rgba(128,0,128,0.2)";
                 break;
               case 9:
-                renderedUser.background = "rgba(0,128,0,0.7)";
+                renderedUser.background = "rgba(0,128,0,0.2)";
                 break;
               case 10:
-                renderedUser.background = "rgba(128,128,128,0.7)";
+                renderedUser.background = "rgba(128,128,128,0.2)";
                 break;
               case 0:
-                renderedUser.background = "rgba(237,237,79,0.7)";
+                renderedUser.background = "rgba(237,237,79,0.2)";
                 break;
             }
 
